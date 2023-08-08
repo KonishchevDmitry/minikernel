7513dd42e767e96568c64dfe032688aecfa67430
Dmitry Konishchev <konishchev@gmail.com>
2023-08-08 20:57:08 +0300

    Introduce inb()/outb()

--- kernel/misc.c
+++ kernel/misc.c
@@ -1,5 +1,22 @@
 #include <types.h>
 
+u8 inb(u16 port) {
+    u8 result;
+    asm volatile (
+        "in %%dx, %%al"
+        : "=a"(result)
+        : "d"(port)
+    );
+    return result;
+}
+
+void outb(u16 port, u8 value) {
+    asm volatile (
+        "out %%al, %%dx"
+        :: "a"(value), "d"(port)
+    );
+}
+
 void halt() {
     asm volatile ("cli");
 
--- kernel/misc.h
+++ kernel/misc.h
@@ -1,6 +1,11 @@
 #ifndef MISC_H
 #define MISC_H
 
+#include <types.h>
+
+u8 inb(u16 port);
+void outb(u16 port, u8 value);
+
 void halt();
 
 #endif
